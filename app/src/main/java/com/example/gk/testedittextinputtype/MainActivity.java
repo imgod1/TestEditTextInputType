6403c4612b17821cf97927f0f58b9963165b5ad6
gaokang <970720947@qq.com>
2016-08-24 10:10:35 +0800

    edittext inputtype test

---
+++ app/src/main/java/com/example/gk/testedittextinputtype/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.gk.testedittextinputtype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etxt_txt_pwd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        etxt_txt_pwd = (EditText) findViewById(R.id.etxt_txt_pwd);
+        etxt_txt_pwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.e("test", "text password:" + s.toString());
+            }
+        });
+    }
+}
